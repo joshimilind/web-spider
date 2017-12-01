fed6406bfce51a775e404b06a42073b8a946bc35
Milind <milind.joshi@synerzip.com>
2017-12-01 17:52:42 +0530

    impure

--- src/main/scala/web/crawler/net/ClientActor.scala
+++ src/main/scala/web/crawler/net/ClientActor.scala
@@ -1,18 +1,16 @@
 package web.crawler.net
 
 import akka.actor.{ActorRef, ActorSystem}
-
 object ClientActor extends App {
 
   val _system: ActorSystem = ActorSystem.create("hello-system")
-  val actor: ActorRef = _system.actorOf(ServerActor.props)
-  val url: String =
-    ("https://www.google.co.in/")
 
-  actor ! url
+  val serveractor: ActorRef = _system.actorOf(ServerActor.props)
+  val url: String = "https://www.google.co.in/"
+
+  serveractor ! url
 
   Thread.sleep(2000)
 
-  _system.shutdown();
-  _system.awaitTermination();
+  _system.terminate
 }
--- src/main/scala/web/crawler/net/ServerActor.scala
+++ src/main/scala/web/crawler/net/ServerActor.scala
@@ -1,25 +1,31 @@
 package web.crawler.net
+
 import akka.actor.{Actor, Props}
 import org.jsoup.Jsoup
+import org.jsoup.select.Elements
+import scala.collection.JavaConversions._
 
 class ServerActor extends Actor {
-//  def props = Props(classOf[ServerActor])
-
   def crawl(url: String): Unit = {
     println("\n" + url)
-
-    val doc = Jsoup.connect(url).get()
-    val title = doc.title()
-    val links = doc.select("a[href]")
-
-    println(s"\n \t title :  + $title")
-    println(s" \t links :  + $links")
+    try {
+      val doc = Jsoup.connect(url).get()
+      val title = doc.title()
+      val links: Elements = doc.select("a[href]")
+      for (link <- links) {
+        println(s"\ntitle : " + link.text)
+        println("link  : " + link.attr("abs:href"))
+      }
+    } catch {
+      case _: Throwable =>
+        println(
+          "\n Something went wrong, may be web site has disabled robot.txt!")
+    }
   }
-  override def receive: Receive = {
+  def receive: Receive = {
     case url => crawl(s"url:$url")
     case _   => unhandled()
   }
-
 }
 
 object ServerActor {
