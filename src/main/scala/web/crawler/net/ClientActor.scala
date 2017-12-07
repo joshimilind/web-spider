6172bd7a0176e58380189aab17370ac174f3acc0
Milind <milind.joshi@synerzip.com>
2017-12-07 15:00:16 +0530

    crawling

--- src/main/scala/web/crawler/net/ClientActor.scala
+++ src/main/scala/web/crawler/net/ClientActor.scala
@@ -1,8 +1,47 @@
+/*
 package web.crawler.net
+import akka.actor._
+import akka.actor.{Actor, ActorRef, Props}
+import web.crawler.net.ClientActor.{StartCrawling, CrawledUrls}
+import web.crawler.net.LinkChecker.Result
+import scala.collection.mutable
+object ClientActor {
+  case class StartCrawling(url: String, depth: Int) {}
+  case class CrawledUrls(url: String, links: Set[String]) {}
+}
+class ClientActor extends Actor {
 
-import akka.actor.{ActorRef, ActorSystem}
-object ClientActor extends App {
+  val clients: mutable.Map[String, Set[ActorRef]] =
+    collection.mutable.Map[String, Set[ActorRef]]()
+  val controllers: mutable.Map[String, ActorRef] =
+    mutable.Map[String, ActorRef]()
 
+  def receive = {
+
+    case StartCrawling(url, depth) =>
+      val controller = controllers get url
+      if (controller.isEmpty) {
+        controllers += (url -> context.actorOf(
+          Props[LinkChecker](new LinkChecker(url, depth))))
+        clients += (url -> Set.empty[ActorRef])
+      }
+      clients(url) += sender
+
+    case Result(url, links) =>
+      context.stop(controllers(url))
+
+      clients(url) foreach (_ ! CrawledUrls(url, links))
+      clients -= url
+
+      controllers -= url
+  }
+}
+
+
+
+
+
+/*
   val _system: ActorSystem = ActorSystem.create("hello-system")
 
   val serveractor: ActorRef = _system.actorOf(ServerActor.props)
@@ -13,4 +52,5 @@ object ClientActor extends App {
   Thread.sleep(2000)
 
   _system.terminate
-}
+}*/
+*/
--- src/main/scala/web/crawler/net/StartingPoint.scala
+++ src/main/scala/web/crawler/net/StartingPoint.scala
@@ -1,7 +1,6 @@
 package web.crawler.net
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import web.crawler.net.ServerActor.{CrawledUrls, StartCrawling}
-import web.crawler.net.Client._system
 
 class Client(StartPoint: ActorRef, url: String, depth: Int) extends Actor {
   StartPoint ! StartCrawling(url, depth)
@@ -10,17 +9,17 @@ class Client(StartPoint: ActorRef, url: String, depth: Int) extends Actor {
     case CrawledUrls(root, links) =>
       println(s"root link: $root \ncrawled with depth: $depth \ncrawled Links: \n${links.toList.mkString("\n")}")
 
-      println(s"Current Time ${System.currentTimeMillis}")
 
       Thread.sleep(1000)
-      _system.terminate()
+//      _system.terminate()
   }
 }
 object Client extends App {
-
-  println(s"Current Time ${System.currentTimeMillis}")
+  println("...")
   val _system = ActorSystem("webSpider")
   val StartPoint = _system.actorOf(Props[ServerActor], "webSpider")
   val client = _system.actorOf(
-    Props[Client](new Client(StartPoint, "http://www.google.com/", 1)))
+    Props[Client](new Client(StartPoint, "http://www.google.co.in/", 2)))
+//  _system.terminate()
+
 }
